811f0e9ee3fb8352a9f446171148fb347698cac6
Christian Martin <cmartin60@academic.rrc.ca>
2025-01-14 09:46:35 -0600

    Commit: Set-up API documentation with Swagger.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 import express, { Express } from "express";
+import setupSwagger from "../config/swagger";
 
-// Initialize Express application
 const app: Express = express();
 
-// Define a route
+setupSwagger(app);
+
 app.get("/", (req, res) => {
-	res.send("Hello, World!");
+    res.send("Hello World!");
+});
+
+/**
+ * @openapi
+ * /tasks:
+ *   get:
+ *     summary: Retrieve a list of tasks
+ *     tags: [Tasks]
+ *     responses:
+ *       200:
+ *         description: A list of tasks
+ */
+app.get("/tasks", (req, res) => {
+	res.send("Retrieve tasks");
 });
 
 export default app;
